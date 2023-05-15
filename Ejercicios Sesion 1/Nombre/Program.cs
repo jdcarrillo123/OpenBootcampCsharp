2a0b652fbe153a2274a8e0e8a901a1866c6f7f69
jdcarrillo123 <dcarrillo@idea.gob.mx>
2023-05-15 11:49:06 -0600

    obtiene nombre y lo muestra en consola

---
+++ Ejercicios Sesion 1/Nombre/Program.cs
@@ -0,0 +1,7 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+Console.WriteLine("Escribe tu nombre");
+string? nombre;
+nombre=Console.ReadLine();
+Console.WriteLine("Hello, "+nombre);
+
